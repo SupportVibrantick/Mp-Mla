a36fb4d43fe8460bf70ac01d2a86a5b857762dfd
Mayank Goyal <2004mayankgoyal@gmail.com>
2026-03-11 13:52:48 +0530

    fix in url

--- frontend/client/src/lib/api.ts
+++ frontend/client/src/lib/api.ts
@@ -1,8 +1,7 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from "axios";
 import { TokenStorage } from "./auth";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = import.meta.env.VITE_API_URL;
 
 // Create axios instance
 const api = axios.create({
