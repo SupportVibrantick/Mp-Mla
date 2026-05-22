45f7f00a0a63a79f700b9b10ea96ba498d3898e5
Mayank Goyal <2004mayankgoyal@gmail.com>
2026-05-22 15:45:52 +0530

    add deleted

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,24 +3,18 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/", // 👈 ADD THIS
-
   plugins: [react()],
-  root: "client",
-
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
+      "@": path.resolve(__dirname, "./src"),
       "@shared": path.resolve(__dirname, "./shared"),
     },
   },
-
   server: {
     port: 5173,
   },
-
   build: {
-    outDir: "../dist",
+    outDir: "dist",
     emptyOutDir: true,
   },
 });
